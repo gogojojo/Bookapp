673e5461ab93e0986630ec50b6782675c1e6bbc7
gogojojo <joseph.gochua@gmail.com>
2015-01-15 18:22:26 -0800

    Created a form for users to add books, user can now add books
    [#85808684]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,36 @@
+class BooksController < ApplicationController
+		before_action :authorize
+	def index
+ 		@books = Book.all
+ 	end
+ 	def show 
+ 		@book = Book.find(params[:id])
+ 	end
+ 	def new
+ 		@book = Book.new
+ 	end
+ 	def create 
+ 		@book = Book.new(params.require(:book).permit(:title, :pages))
+
+ 		if @book.save
+ 			redirect_to user_path(current_user.id)
+ 			@book.user_ids.push(current_user.id)
+ 			current_user.books.push(@book)
+ 		else
+ 			render "new"
+ 		end
+ 	end
+ 	def edit
+ 		@book = Book.find(params[:id])
+ 	end
+ 	def update
+ 		@book = Book.find(params[:id])
+ 		@book.update_attributes(params.require(:book).permit(:title, :pages))
+			redirect_to book_path(@book.id)
+ 	end
+ 	def destroy
+ 		@book = book.find(params[:id])
+ 		@book.destroy 
+ 		redirect_to user_path(current_user.id)
+ 	end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book
   include Mongoid::Document
   field :title
-  field :Pages
+  field :pages
   has_and_belongs_to_many :users
   belongs_to :author
 end
