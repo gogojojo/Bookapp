220e52788335a208655bef0b7b204a3734b24033
gogojojo <joseph.gochua@gmail.com>
2015-01-21 19:42:49 -0800

    added figaro

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 				session[:user_id] = user.id 
 				redirect_to user_path(user.id)
 			else
-				flash[:error] = 'Email or passowrd incorrect'
+				flash[:error] = 'Email or passowrd incorrects'
 				redirect_to root_path
 			end
 	end
