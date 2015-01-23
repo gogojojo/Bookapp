db6c6c5041ce0c087c2d29c186781b638d16b846
gogojojo <joseph.gochua@gmail.com>
2015-01-23 11:36:52 -0800

    fixed current finished button

--- app/controllers/currents_controller.rb
+++ app/controllers/currents_controller.rb
@@ -7,7 +7,6 @@ class CurrentsController < ApplicationController
 		@current = Current.new(params.require(:current).permit(:title, :author, :pages, :pages_read))
 		current_user.current = @current
 		if current_user.current.save
-			current_user.current = @current
 			flash[:success] = 'Added a currently reading book'
 			redirect_to user_path(current_user)
 		else
@@ -23,9 +22,11 @@ class CurrentsController < ApplicationController
 			redirect_to user_path(current_user)
  	end
  	def finished
- 		@book = Book.new(title: current_user.current.title)
- 		current_user.books.push(@book)
- 		current_user.current = nil
- 		redirect_to user_path(current_user.id)
+ 		book = Book.new(title: current_user.current.title, author: current_user.current.title, pages: current_user.current.pages)
+ 		book.save
+	 		current_user.books.push(book)
+	 		current_user.current = nil
+	 		redirect_to user_path(current_user.id)
+	 	
  	end
 end
