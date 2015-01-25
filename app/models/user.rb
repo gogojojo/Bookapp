113d7b9b518acb55666b5af3c15f70ab94c74fc1
gogojojo <joseph.gochua@gmail.com>
2015-01-25 14:34:17 -0800

    front page is finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
   field :name
-  field :username
   field :email
   field :password_digest
 
@@ -15,7 +14,6 @@ class User
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true
-  validates :username, presence: true, uniqueness: true
 
 
   validates :password, confirmation: true
