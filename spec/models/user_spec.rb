2459674353c3429271d028a219ad869402afff37
gogojojo <joseph.gochua@gmail.com>
2015-02-26 11:44:32 -0800

    removing some tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,1 @@
 require 'rails_helper'
-	RSpec.describe User do 
-		it "validates that the user email exists" do
-			@user = User.new(
-				name:'joseph',
-				email: nil,
-				username:'Coolkat',
-				password:'password',
-				password_confirmation:'password'
-				)
-			expect(@user.valid?).to eq(false)
-		end
-	end
